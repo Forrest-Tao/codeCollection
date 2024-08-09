27ef13b91e339847e719177619624c9620de0ca3
Forreste <2674978072@qq.com>
2024-08-09 21:47:42 +0800

    update：在N reader M writer条件下，close channel

--- channel/main.go
+++ channel/main.go
@@ -1,26 +1,138 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"math/rand"
+	"strconv"
+	"sync"
+	"time"
 )
 
 func main() {
-	ch := make(chan int)
-	//close(ch) // 关闭 channel
+	rand.Seed(time.Now().UnixNano()) // needed before Go 1.20
+	log.SetFlags(0)
 
-	// 尝试从关闭的 channel 中接收数据
-	value, ok := <-ch
-	if !ok {
-		fmt.Println("Channel is closed.")
-	} else {
-		fmt.Printf("Received value: %d\n", value)
+	// ...
+	const Max = 100000
+	const NumReceivers = 10
+	const NumSenders = 1000
+
+	wgReceivers := sync.WaitGroup{}
+	wgReceivers.Add(NumReceivers)
+
+	// ...
+	dataCh := make(chan int)
+	stopCh := make(chan struct{})
+	// stopCh is an additional signal channel.
+	// Its sender is the moderator goroutine shown
+	// below, and its receivers are all senders
+	// and receivers of dataCh.
+	toStop := make(chan string, 1)
+	// The channel toStop is used to notify the
+	// moderator to close the additional signal
+	// channel (stopCh). Its senders are any senders
+	// and receivers of dataCh, and its receiver is
+	// the moderator goroutine shown below.
+	// It must be a buffered channel.
+
+	var stoppedBy string
+
+	// moderator
+	go func() {
+		stoppedBy = <-toStop
+		close(stopCh)
+	}()
+
+	// senders
+	for i := 0; i < NumSenders; i++ {
+		go func(id string) {
+			for {
+				value := rand.Intn(Max)
+				if value == 0 {
+					// Here, the try-send operation is
+					// to notify the moderator to close
+					// the additional signal channel.
+					select {
+					case toStop <- "sender#" + id:
+					default:
+					}
+					return
+				}
+
+				// The try-receive operation here is to
+				// try to exit the sender goroutine as
+				// early as possible. Try-receive and
+				// try-send select blocks are specially
+				// optimized by the standard Go
+				// compiler, so they are very efficient.
+				select {
+				case <-stopCh:
+					return
+				default:
+				}
+
+				// Even if stopCh is closed, the first
+				// branch in this select block might be
+				// still not selected for some loops
+				// (and for ever in theory) if the send
+				// to dataCh is also non-blocking. If
+				// this is unacceptable, then the above
+				// try-receive operation is essential.
+				select {
+				case <-stopCh:
+					return
+				case dataCh <- value:
+				}
+			}
+		}(strconv.Itoa(i))
 	}
 
-	// 尝试再接收一次
-	value, ok = <-ch
-	if !ok {
-		fmt.Println("Channel is closed.")
-	} else {
-		fmt.Printf("Received value: %d\n", value)
+	// receivers
+	for i := 0; i < NumReceivers; i++ {
+		go func(id string) {
+			defer wgReceivers.Done()
+
+			for {
+				// Same as the sender goroutine, the
+				// try-receive operation here is to
+				// try to exit the receiver goroutine
+				// as early as possible.
+				select {
+				case <-stopCh:
+					return
+				default:
+				}
+
+				// Even if stopCh is closed, the first
+				// branch in this select block might be
+				// still not selected for some loops
+				// (and forever in theory) if the receive
+				// from dataCh is also non-blocking. If
+				// this is not acceptable, then the above
+				// try-receive operation is essential.
+				select {
+				case <-stopCh:
+					return
+				case value := <-dataCh:
+					if value == Max-1 {
+						// Here, the same trick is
+						// used to notify the moderator
+						// to close the additional
+						// signal channel.
+						select {
+						case toStop <- "receiver#" + id:
+						default:
+						}
+						return
+					}
+
+					log.Println(value)
+				}
+			}
+		}(strconv.Itoa(i))
 	}
+
+	// ...
+	wgReceivers.Wait()
+	log.Println("stopped by", stoppedBy)
 }
