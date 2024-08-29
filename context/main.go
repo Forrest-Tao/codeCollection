9dfa957a414d34ea95de4b4e8a2a863e37822aa5
Forreste <2674978072@qq.com>
2024-08-29 22:07:21 +0800

    add：自己瞎想的 context高阶用法

--- context/main.go
+++ context/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"sync"
 )
 
 // withAsyncCancel 装饰器函数，用来封装 goroutine 逻辑并处理错误和取消操作
@@ -32,4 +33,43 @@ func main() {
 	cancel()
 	cancel()
 	fmt.Println("----")
+
+	run()
+}
+
+func handle(wg *sync.WaitGroup, ch chan string, ctx context.Context, cancelFunc context.CancelFunc) {
+	tag := 0
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case v := <-ch:
+				fmt.Println(v)
+			case <-ctx.Done():
+				cancelFunc()
+				tag = 1
+			}
+			fmt.Println("out")
+			if tag == 1 {
+				break
+			}
+		}
+	}()
+}
+
+func run() {
+	var (
+		wg  sync.WaitGroup
+		ctx = context.Background()
+	)
+
+	newctx, cancel := context.WithCancel(ctx)
+	ch := make(chan string, 1)
+	wg.Add(1)
+	handle(&wg, ch, newctx, cancel)
+	for i := range 3 {
+		ch <- fmt.Sprintf("--%d--\n", i)
+	}
+	cancel()
+	wg.Wait()
 }
